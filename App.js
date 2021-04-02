a7aa628411dfe09e8195a82649325aee096ccde1
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2021-04-02 18:27:27 +0900

    before setting react navigations

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import * as Font from "expo-font"
+import { Ionicons } from "@expo/vector-icons"
 import { StyleSheet, Text, View } from 'react-native';
+import { Asset } from 'expo-asset';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preLoad = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsoLoad.map(font => Font.loadAsync(font));
+    const imagesToLoad = [require("./assets/icon.png")];
+    const imagePromises = imagesToLoad.map(image => Asset.loadAsync(image));
+    return Promise.all([
+      ...fontPromises,
+      ...imagePromises
+    ]);
+  }
+  if (loading){
+    return <AppLoading
+      startAsync={preLoad}
+      onError={console.warn}
+      onFinish={onFinish}
+    />
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
