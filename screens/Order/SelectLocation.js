1ca976d86402fc8b82ecb47d874045756ba618ff
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2021-05-01 19:06:51 +0900

    commit before expo upgrade

--- screens/Order/SelectLocation.js
+++ screens/Order/SelectLocation.js
@@ -1,8 +1,27 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import MapView from 'react-native-maps';
+import { Alert, Text, View } from "react-native";
+import * as Location from 'expo-location';
+import constants from "../../constants";
 
 export default () => {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    useEffect(() => {
+        (async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        Alert.alert("1!");
+        if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+        })();
+    }, []);
+    console.log(location);
     return <View>
-        <Text>select location</Text>
+        <MapView style={{width : constants.width, height : constants.width}}/>
     </View>
 }
