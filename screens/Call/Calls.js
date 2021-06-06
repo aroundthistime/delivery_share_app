7579037b3662c219f233b703157b33a604b7aa0e
KG <acrkdduf@naver.com>
2021-06-06 21:57:46 +0900

    fetch real data from database by graphQL in Calls Component

--- screens/Call/Calls.js
+++ screens/Call/Calls.js
@@ -1,133 +1,41 @@
-import React from "react";
-import { FlatList, ScrollView, TouchableOpacity } from "react-native";
+import { useQuery } from "@apollo/client";
+import React, { useState } from "react";
+import {
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+  View,
+  Image,
+} from "react-native";
 import CallListBar from "../../components/CallListBar";
-
-const dummyCallsData = [
-  {
-    image:
-      "https://user-images.githubusercontent.com/48883344/116443339-8380a280-a88e-11eb-8565-275fd79c0003.PNG",
-    userId: "분홍벚꽃",
-    brandName: "스타벅스",
-    menus: [
-      { id: "#&112233", name: "아이스 아메리카노", price: 25000 },
-      { id: "#&342233", name: "핫 아메리카노", price: 10000 },
-      { id: "#&534633", name: "바닐라 아메리카노", price: 600 },
-    ],
-    dist: 10,
-    requestToRestaurant: "",
-    requestToUser: "저는 반찬 필요 없어요"
-  },
-  {
-    image:
-      "https://user-images.githubusercontent.com/48883344/116443343-84b1cf80-a88e-11eb-9e64-438c0ab26dca.PNG",
-    userId: "노랑백합",
-    brandName: "커피빈",
-    menus: [
-      { id: "#&534633", name: "핫 아메리카노", price: 10000 },
-      { id: "#&534643", name: "바닐라 아메리카노", price: 600 },
-    ],
-    dist: 20,
-    requestToRestaurant: "",
-    requestToUser: "저는 반찬 필요 없어요"
-  },
-  {
-    image:
-      "https://user-images.githubusercontent.com/48883344/116443345-854a6bold-a88e-11eb-9684-4ef24d1ed275.PNG",
-    userId: "검은튤립",
-    brandName: "앤젤인어스",
-    menus: [
-      { id: "#&534633", name: "바닐라 아메리카노", price: 600 },
-      { id: "#&134633", name: "수박 아메리카노", price: 4500 },
-    ],
-    dist: 30,
-    requestToRestaurant: "",
-    requestToUser: "저는 반찬 필요 없어요"
-  },
-  {
-    image:
-      "https://user-images.githubusercontent.com/48883344/116443349-854a6bold-a88e-11eb-87e0-36f0404304e6.PNG",
-    userId: "빨강수선화",
-    brandName: "탐앤탐스",
-    menus: [
-      { id: "#&534633", name: "수박 아메리카노", price: 4500 },
-      { id: "#&15533", name: "바닐라 아메리카노", price: 600 },
-    ],
-    dist: 40,
-    requestToRestaurant: "",
-    requestToUser: "저는 반찬 필요 없어요"
-  },
-  {
-    image:
-      "https://user-images.githubusercontent.com/48883344/116443352-85e2fc80-a88e-11eb-9c02-b7d03d1b8767.PNG",
-    userId: "주황철쭉",
-    brandName: "코파인",
-    menus: [
-      { id: "#&534633", name: "참외 아메리카노", price: 4500 },
-      { id: "#&543633", name: "수박 아메리카노", price: 4500 },
-    ],
-    dist: 50,
-    requestToRestaurant: "",
-    requestToUser: "저는 반찬 필요 없어요"
-  },
-  {
-    image:
-      "https://user-images.githubusercontent.com/48883344/116443354-85e2fc80-a88e-11eb-9e44-f0b4d009989a.PNG",
-    userId: "초록할미꽃",
-    brandName: "할리스커피",
-    menus: [
-      { id: "#&534633", name: "딸기 아메리카노", price: 4500 },
-      { id: "#&53113", name: "버섯 아메리카노", price: 4500 },
-    ],
-    dist: 60,
-    requestToRestaurant: "",
-    requestToUser: "저는 반찬 필요 없어요"
-  },
-  {
-    image:
-      "https://user-images.githubusercontent.com/48883344/116443358-867b9300-a88e-11eb-9a83-19b842b9db0a.PNG",
-    userId: "보라개나리",
-    brandName: "이디야",
-    menus: [{ id: "#534633", name: "버섯 아메리카노", price: 4500 }],
-    dist: 70,
-    requestToRestaurant: "",
-    requestToUser: "저는 반찬 필요 없어요"
-  },
-  {
-    image:
-      "https://user-images.githubusercontent.com/48883344/116443360-867b9300-a88e-11eb-96c4-03d76c146aed.PNG",
-    userId: "하양목련",
-    brandName: "카페베네",
-    menus: [
-      { id: "#&534633", name: "무궁화 아메리카노", price: 4500 },
-      { id: "#&542333", name: "버섯 아메리카노", price: 4500 },
-    ],
-    dist: 80,
-    requestToRestaurant: "",
-    requestToUser: "저는 반찬 필요 없어요"
-  },
-  {
-    image:
-      "https://user-images.githubusercontent.com/48883344/116443364-87142980-a88e-11eb-8ca3-1cfc88cb44e8.PNG",
-    userId: "파랑아카시아",
-    brandName: "투썸플레이스",
-    menus: [
-      { id: "#&534633", name: "아프리카 아메리카노", price: 4500 },
-      { id: "#&124633", name: "버섯 아메리카노", price: 4500 },
-    ],
-    dist: 90,
-    requestToRestaurant: "",
-    requestToUser: "저는 반찬 필요 없어요"
-  },
-];
+import { GET_NEAR_CALLINGS } from "../../queries/CallingsQueries";
+import { ButtonText, RefreshBtn } from "./styled";
 
 /**
  * TODO *
- * 1. 데이터베이스 연결하여 실제 데이터 연동 작업 필요
+ * 1. 데이터베이스 연결하여 실제 데이터 연동 작업 필요 (✔)
  * 2. 거리 순으로 오름차순 정렬하여 가장 가까운 거리부터 출력하도록 구현 (✔)
- * 3. 2번 작업을 서버에서 할 지, 클라이언트에서 할 지 논의 필요 (지금은 클라이언트에서 적용 중)
+ * 3. Emulation Geolocation 기능 인식 문제로 고정 lat, lng 값 사용중 (현재 내 위치 기반으로 수정 필요)
  */
 
+// Emulator에서 Geolocation 기능이 안 먹는 관계로 고정 lat, lng 값 사용
+const locObj = {
+  latitude: 37.59685279191918,
+  longitude: 127.06075479761786,
+};
+
 export default ({ navigation }) => {
+  const [refresh, setRefresh] = useState(false);
+  const { loading, data, refetch } = useQuery(GET_NEAR_CALLINGS, {
+    variables: locObj,
+  });
+
+  const handleRefresh = () => {
+    refetch();
+    setRefresh((prev) => !prev);
+  };
+
   const renderCall = ({ item }) => (
     <TouchableOpacity
       activeOpacity={0.8}
@@ -136,13 +44,36 @@ export default ({ navigation }) => {
       <CallListBar {...item} />
     </TouchableOpacity>
   );
+
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <FlatList
-        data={dummyCallsData.sort((a, b) => a.dist - b.dist)}
-        renderItem={renderCall}
-        nestedScrollEnabled
-      ></FlatList>
-    </ScrollView>
+    <View style={{ flex: 1 }}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {loading ? (
+          <View
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              height: 300,
+            }}
+          >
+            <Image
+              style={{ width: 40, height: 40 }}
+              source={require("../../assets/loading.gif")}
+            />
+          </View>
+        ) : (
+          <FlatList
+            data={data.getNearCalls
+              .filter((call) => call.status === "isActivated")
+              .sort((a, b) => a.distance - b.distance)}
+            renderItem={renderCall}
+            nestedScrollEnabled
+          ></FlatList>
+        )}
+      </ScrollView>
+      <RefreshBtn onPress={handleRefresh}>
+        <ButtonText>새로고침</ButtonText>
+      </RefreshBtn>
+    </View>
   );
 };
