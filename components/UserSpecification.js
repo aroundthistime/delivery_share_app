039701f437744f2a94043f32469d1a6a85d69b8b
KG <acrkdduf@naver.com>
2021-06-06 22:11:27 +0900

    connect real database from server by graphQL

--- components/UserSpecification.js
+++ components/UserSpecification.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Text, View } from "react-native";
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome } from "@expo/vector-icons";
 import styles from "../styles";
 
 /**
@@ -10,21 +10,28 @@ import styles from "../styles";
  * 2. User Star Rank 연동 필요
  */
 
-const UserSpecification = ({ userId }) => {
+const UserSpecification = ({ user }) => {
+  const thumbnail =
+    user.thumbnail ||
+    "https://user-images.githubusercontent.com/48883344/116450778-a9aa4080-a896-11eb-8515-ad4a8af7d342.png";
   return (
     <UserSpecs>
       <ProfileImage
         source={{
-          uri:
-            "https://user-images.githubusercontent.com/48883344/116450778-a9aa4080-a896-11eb-8515-ad4a8af7d342.png",
+          uri: thumbnail,
         }}
       />
       <View>
         <Text style={{ marginBottom: 10, fontWeight: "bold", fontSize: 18 }}>
-          {userId}
+          {user.ID}
         </Text>
-        {/* <Text>유저평점: ⭐⭐⭐⭐</Text> */}
-        <Text style={{ textAlignVertical: "center" }}>유저평점 : <FontAwesome name="star" size={13.5} color={styles.yellowColor} /> 3.6</Text>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text style={{ textAlignVertical: "center", marginRight: 10 }}>
+            유저평점 :
+          </Text>
+          <FontAwesome name="star" size={13.5} color={styles.yellowColor} />
+          <Text style={{ marginLeft: 5 }}>{user.rate.toFixed(1)}</Text>
+        </View>
       </View>
     </UserSpecs>
   );
